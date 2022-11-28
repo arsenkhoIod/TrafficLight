ecaba05ce00ca10d2be912d88aa2e9b4c66544c8
arsenkhoIod <arsenykholod@yandex.ru>
2022-11-29 00:32:44 +0300

    work with logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
     }
-
+    
+    @IBAction func greetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+//        if greetingLabel.isHidden {
+//            greetingButton.setTitle("Show Greeting", for: .normal)
+//        } else {
+//            greetingButton.setTitle("Hide Greeting", for: .normal)
+//        }
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+    }
+    
 
 }
 
